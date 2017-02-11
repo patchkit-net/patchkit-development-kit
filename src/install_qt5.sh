9127bc9aab4cbec5c787a8ac3a26147547f47959
Tomasz Jaworski <tomasz.jaworski98@gmail.com>
2017-02-11 19:18:40 +0100

    Fix Qt5 installation on OSX

--- src/install_qt5.sh
+++ src/install_qt5.sh
@@ -11,10 +11,12 @@ function error()
 
 echo Installing Qt5...
 
-pushd $SRC_INSTALL_QT5_SCRIPT_DIR/qt5
+mkdir $PDK_INSTALL_TEMP_DIR/qt5
+
+pushd $PDK_INSTALL_TEMP_DIR/qt5
 
 # Configure Qt
-./configure \
+$SRC_INSTALL_QT5_SCRIPT_DIR/qt5/configure \
   -prefix $PDK_INSTALL_PLATFORM_DIR/qt5 \
   -debug-and-release \
   -opensource \
@@ -28,14 +30,14 @@ pushd $SRC_INSTALL_QT5_SCRIPT_DIR/qt5
   -qt-freetype \
   -qt-harfbuzz \
   -qt-pcre \
-  -qt-xcb \
   -qt-xkbcommon-x11 \
-  -qpa xcb \
+  -no-xcb \
   -nomake examples \
   -nomake tests \
   -silent \
   -fontconfig \
   -opengl \
+  -pkg-config \
 
 # Build Qt5
 make
